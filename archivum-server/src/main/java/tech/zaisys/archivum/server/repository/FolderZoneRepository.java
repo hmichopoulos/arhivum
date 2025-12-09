c608666b05bb2595a293e30c652a2af657b67757
Haris <hmichopoulos@gmail.com>
2025-12-09 12:24:30 +0200

    Remove unused imports in FolderZoneRepository
    Remove unused Query and Param imports that are not needed
    since the repository uses Spring Data JPA derived query methods.

--- archivum-server/src/main/java/tech/zaisys/archivum/server/repository/FolderZoneRepository.java
+++ archivum-server/src/main/java/tech/zaisys/archivum/server/repository/FolderZoneRepository.java
@@ -2,8 +2,6 @@ package tech.zaisys.archivum.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import tech.zaisys.archivum.server.domain.FolderZone;
